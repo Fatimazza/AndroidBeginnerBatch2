366e0ccf9424f93af741b78df4708198040b1249
Fatima Azzahro <fatima@paypro.id>
2017-07-11 07:43:38 +0700

    [Fatimazza][Pertemuan 4] Layout inflater practices

--- app/src/main/java/com/example/android/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/android/myapplication/HomeActivity.java
@@ -2,6 +2,9 @@ package com.example.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        LayoutInflater inflater = getLayoutInflater();
+        LayoutInflater inflater2 = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+//        inflater2.inflate(R.layout.layout_tambahan, (LinearLayout) findViewById(R.id.llhome));
+
+        LinearLayout layout = (LinearLayout) findViewById(R.id.llhome);
+        View view = inflater.inflate(R.layout.layout_tambahan, (LinearLayout) findViewById(R.id.llhome), false);
+        layout.addView(view);
     }
 }
