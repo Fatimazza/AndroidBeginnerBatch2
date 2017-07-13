0739a7062781d042d9a2289b109b876ddbdaac78
Fatima Azzahro <fatima@paypro.id>
2017-07-13 07:52:51 +0700

    [Fatimazza][Pertemuan 5] Place Fragment on Frame, Fragment Interaction through Activity

--- app/src/main/java/com/example/android/myapplication/FragmentActivity.java
+++ app/src/main/java/com/example/android/myapplication/FragmentActivity.java
@@ -1,13 +1,40 @@
 package com.example.android.myapplication;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FragmentActivity extends AppCompatActivity {
+
+    Button btnActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fragment);
+
+        FragmentOne fragmentOne = new FragmentOne();
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout, fragmentOne);
+        ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+        ft.addToBackStack(null);
+        ft.commit();
+
+        btnActivity = (Button) findViewById(R.id.btnActivity);
+        btnActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTwo fragmentTwo = new FragmentTwo();
+                FragmentTransaction ft = getFragmentManager().beginTransaction();
+                ft.replace(R.id.frameLayout, fragmentTwo);
+                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
+                ft.addToBackStack(null);
+                ft.commit();
+            }
+        });
+
+
     }
 }
