2d094ac648a100d5787c68314cc7700db0c37e68
Fatima Azzahro <fatima@paypro.id>
2017-07-13 07:39:36 +0700

    [Fatimazza][Pertemuan 5] Fragment Interaction with Activity via Toast

--- app/src/main/java/com/example/android/myapplication/FragmentOne.java
+++ app/src/main/java/com/example/android/myapplication/FragmentOne.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by fatimazza on 7/13/17.
@@ -13,10 +16,25 @@ import android.view.ViewGroup;
 
 public class FragmentOne extends Fragment {
 
-    @Nullable
+    EditText etFragment1;
+    Button button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
         Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_one, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_one, container, false);
+
+        etFragment1 = (EditText) view.findViewById(R.id.etFragment1);
+
+        button = (Button) view.findViewById(R.id.btnFragment1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getActivity().getApplicationContext(),
+                    etFragment1.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 }
