eaeb6f88c691242faa4ece0c696951f06556fa42
Fatima Azzahro <fatima@paypro.id>
2017-07-18 08:24:07 +0700

    [Fatimazza][Pertemuan 6] Prepare Activity for Async Task

---
+++ app/src/main/java/com/example/android/myapplication/AsyncTaskActivity.java
@@ -0,0 +1,31 @@
+package com.example.android.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AsyncTaskActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText time;
+    private TextView finalResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_async_task);
+
+        time = (EditText) findViewById(R.id.et_time);
+        button = (Button) findViewById(R.id.btn_run);
+        finalResult = (TextView) findViewById(R.id.tv_result);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
